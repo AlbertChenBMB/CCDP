62c259033c1a3b1030c1e5bca1fc0987e9f2b70c
AlbertChenBMB <albirtle93@gmail.com>
2020-07-25 21:36:56 +0800

    check app position

--- EDA/dash_apps/finished_apps/simpleexample2.py
+++ EDA/dash_apps/finished_apps/simpleexample2.py
@@ -51,6 +51,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
 
+app = DjangoDash('SimpleExample2', external_stylesheets=external_stylesheets)
 
 def generate_table(dataframe, max_rows=10):
     return html.Table([
@@ -68,7 +69,7 @@ def generate_table(dataframe, max_rows=10):
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 #app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app = DjangoDash('SimpleExample2', external_stylesheets=external_stylesheets)
+
 app.layout = html.Div(children=[
     html.H4(children='US Agriculture Exports (2011)'),
     generate_table(df)
