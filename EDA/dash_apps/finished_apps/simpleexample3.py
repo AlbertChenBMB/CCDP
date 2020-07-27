08b1ee5efe391d31346136d24ee2fb063da9b5d4
AlbertChenBMB <albirtle93@gmail.com>
2020-07-27 13:16:35 +0800

    show the interaction plot of gender, date, and CD4

--- EDA/dash_apps/finished_apps/simpleexample3.py
+++ EDA/dash_apps/finished_apps/simpleexample3.py
@@ -6,108 +6,79 @@ import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 from django.db import connection
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from django_plotly_dash import DjangoDash
 
 # Create your views here.
-def hello_view(request):
-    gender = ["gender0","gender1"]
-    genderDataFrame = pd.DataFrame()
-    count = []
-    gender0 = EDAData.objects.values_list('gender').filter(date='2018/10/31', gender='0').count()
-    gender1 = EDAData.objects.values_list('gender').filter(date='2018/10/31', gender='1').count()
+#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+genderDataFrame = pd.DataFrame()
+with connection.cursor() as cursor:
+    cursor.execute('SELECT date FROM EDAData GROUP by date')
+    dataDate = cursor.fetchall()
+date = []
+gender = []
+count = []
+genderCD4 = []
 
-    count.append(gender0)
-    count.append(gender1)
-    genderDataFrame["gender"]=gender
-    genderDataFrame["count"]=count
+for i in range(len(dataDate)):
+    for j in range(6):
+        date.append(dataDate[i][0])
+        date.append(dataDate[i][0])
+        gender.append("gender0")
+        gender0 = EDAData.objects.values_list('gender').filter(date=dataDate[i][0], gender='0', CDLabel4Month=j).count()
+        count.append(gender0)
+        genderCD4.append(j)
 
- 
- 
-    # with connection.cursor() as cursor:
-    #     cursor.execute('SELECT date FROM EDAData GROUP by date')
-    #     dataDate = cursor.fetchall()
-
-
-    return render(request, 'hello_django.html', {
-        'data': "Hello Django ",
-        'current_time': str(datetime.now()),
-        'gender0': '333333',
-        # 'gender0': EDAData.objects.values_list('gender').filter(date='2018/10/31', gender='0').count(),
-        # 'gender1': EDAData.objects.values_list('gender').filter(date='2018/10/31', gender='1').count(),
-        # 'check_list': EDAData.objects.values_list('date', 'number', 'gender', 'age', 'edu', 'homeOwnership', 'job1', 'job2', 'job3', 'job4', 'industry1', 'industry2', 'industry3', 'industry4', 'industry5', 'CDLabel4Month').filter(date='2018/10/31'),
-    })
-
-def show(request):
-    def scatter():
-
+        gender.append("gender1")
+        gender1 = EDAData.objects.values_list('gender').filter(date=dataDate[i][0], gender='1', CDLabel4Month=j).count()
         
-        genderDataFrame = pd.DataFrame()
-        with connection.cursor() as cursor:
-            cursor.execute('SELECT date FROM EDAData GROUP by date')
-            dataDate = cursor.fetchall()
-        date = []
-        gender = []
-        count = []
+        count.append(gender1)
+        genderCD4.append(j)
 
-        for i in range(len(dataDate)):
-            date.append(dataDate[i][0])
-            date.append(dataDate[i][0])
-            gender.append("gender0")
-            gender0 = EDAData.objects.values_list('gender').filter(date=dataDate[i][0], gender='0').count()
-            count.append(gender0)
+genderDataFrame["date"] = date
+genderDataFrame["gender"] = gender
+genderDataFrame["count"] = count
+genderDataFrame["genderCD4"] = genderCD4
 
-            gender.append("gender1")
-            gender1 = EDAData.objects.values_list('gender').filter(date=dataDate[i][0], gender='1').count()
-            count.append(gender1)
 
-        genderDataFrame["date"] = date
-        genderDataFrame["gender"] = gender
-        genderDataFrame["count"] = count
 
-        print(genderDataFrame)
-        fig = px.bar(genderDataFrame, x="date", y="count", color="gender", barmode="group", title='性別圖')
-        plot_div = plot(fig, output_type='div', include_plotlyjs=False)
-        return plot_div
 
-    def genderDist():
 
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = DjangoDash('simpleexample3', external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        id='date-slider',
+        min =0,
+        max=len(dataDate)-1,
+        marks={i: dataDate[i][0] for i in range(len(dataDate))},
+        value=len(dataDate)-1,
         
-        genderDataFrame = pd.DataFrame()
-        with connection.cursor() as cursor:
-            cursor.execute('SELECT date FROM EDAData GROUP by date')
-            dataDate = cursor.fetchall()
-        date = []
-        gender = []
-        count = []
-        genderCD4 = []
+        
+    )
+])
 
-        for i in range(len(dataDate)):
-            for j in range(6):
-                date.append(dataDate[i][0])
-                date.append(dataDate[i][0])
-                gender.append("gender0")
-                gender0 = EDAData.objects.values_list('gender').filter(date=dataDate[i][0], gender='0', CDLabel4Month=j).count()
-                count.append(gender0)
-                genderCD4.append(j)
 
-                gender.append("gender1")
-                gender1 = EDAData.objects.values_list('gender').filter(date=dataDate[i][0], gender='1', CDLabel4Month=j).count()
-                # print(gender1)
-                count.append(gender1)
-                genderCD4.append(j)
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    [Input('date-slider', 'value')])
 
-        genderDataFrame["date"] = date
-        genderDataFrame["gender"] = gender
-        genderDataFrame["count"] = count
-        genderDataFrame["genderCD4"] = genderCD4
+def update_figure(selected_date):
+    newdate=dataDate[selected_date][0]
+    print(newdate)
+    filtered_df = genderDataFrame[genderDataFrame.date==newdate]
+    print()
+    print(selected_date)
+    print(filtered_df.date)
+    fig = px.bar(filtered_df, x="gender", y="count", color="genderCD4", barmode="group", title='CD4性別分布圖')
 
-        print(genderDataFrame)
-        fig = px.bar(genderDataFrame, x="date", y="count", color="genderCD4", barmode="group", title='CD4性別分布圖')
-        plot_div = plot(fig, output_type='div', include_plotlyjs=False)
-        return plot_div
+    fig.update_layout(transition_duration=100)
+    
 
-    context ={
-        'plot1': scatter(),
-        'genderDist': genderDist()
-    }
-
-    return render(request, 'welcome.html', context)
+    return fig
