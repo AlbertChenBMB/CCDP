510e2dbe972540203ed9eb19ba5574e6d8e7696f
AlbertChenBMB <albirtle93@gmail.com>
2020-07-27 11:23:19 +0800

    test simpleexample3

--- EDA/urls.py
+++ EDA/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from EDA.dash_apps.finished_apps import simpleexample
 from EDA.dash_apps.finished_apps import simpleexample2
+from EDA.dash_apps.finished_apps import simpleexample3
 # name 是要告訴說在哪個資料夾底下
 urlpatterns = [
     path('', views.show, name='EDA')
